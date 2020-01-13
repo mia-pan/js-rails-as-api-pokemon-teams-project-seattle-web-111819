f2628e0d40bfee6922dd942763ad9649887f5704
mia-pan <miapanyuewei@gmail.com>
2020-01-12 21:02:31 -0800

    Done.

---
+++ pokemon-teams-backend/app/controllers/application_controller.rb
@@ -0,0 +1,6 @@
+class ApplicationController < ActionController::API
+
+    # you were missing the end for the Class here
+    # added this
+
+end
--- pokemon-teams-frontend/src/index.js
+++ pokemon-teams-frontend/src/index.js
@@ -1,3 +1,120 @@
 const BASE_URL = "http://localhost:3000"
 const TRAINERS_URL = `${BASE_URL}/trainers`
 const POKEMONS_URL = `${BASE_URL}/pokemons`
+let editedNode = null
+document.addEventListener("DOMContentLoaded", () => {
+  // showTrainers();
+    })
+fetch(TRAINERS_URL)
+.then(res => res.json())
+.then(jsonData => showTrainers(jsonData))
+//will show all teams
+const showTrainers = (trainerData) => {
+trainerData.forEach(trainer => {
+    newTrainer(trainer)
+    })
+}
+// appends new card to the thing we want
+const newTrainer = (trainer) => {
+  const main = document.querySelector("main"); //where to append it
+  const div = makeTeamCard(trainer); //creates div for team card
+  main.appendChild(div); // sticks it to main (location)
+}
+// constructs single card
+const makeTeamCard = (trainer) => {   //team.id same as the trainers id
+  let div = document.createElement("div");
+  div.classList.add("card");
+  div.setAttribute("data-id", trainer.id); 
+  let pTag = document.createElement("p");
+  pTag.innerText = trainer.name; 
+  let editTrainerButton = document.createElement("button");
+  editTrainerButton.textContent = "Edit Trainer";
+  editTrainerButton.addEventListener("click", () => {
+    let editForm = document.getElementById("edit-form");
+    editForm.style.display = "block"; 
+    editForm.name.value = trainer.name;
+    editForm["trainer-id"].value = trainer.id;
+    editedNode = div;
+  })
+  let addPokemonButton = document.createElement("button");
+  addPokemonButton.textContent = "Add Pokemon";
+  addPokemonButton.setAttribute("data-trainer-id", trainer.id );
+  addPokemonButton.addEventListener("click", () => {
+    postPokemon(trainer);
+    editedNode = div;
+    console.log("add pokemon button working")
+  })
+  let ul = document.createElement("ul");
+  console.log(trainer)
+  trainer.pokemons.forEach(pokemon => {
+    let listItem = document.createElement('li');
+    listItem.innerText = `${pokemon.nickname} (${pokemon.species})`;
+    let releasePokemonButton = document.createElement("button");
+    releasePokemonButton.innerText = "Release";
+    releasePokemonButton.classList.add("release");
+    releasePokemonButton.addEventListener('click', () => {
+      releasePokemon(pokemon)
+      editedNode = div; //sets the editedNode to div. DOM.
+      console.log("release buttong working")
+    })
+    ul.appendChild(listItem);
+    listItem.appendChild(releasePokemonButton);
+  })
+  div.appendChild(pTag);
+  div.appendChild(ul);
+  div.appendChild(addPokemonButton);
+  div.appendChild(editTrainerButton);
+  return div;
+}
+const postPokemon = (trainer) => {
+  fetch(POKEMONS_URL, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify({
+      trainer_id: trainer.id
+    })
+  })
+  .then(res => res.json())
+  .then(trainer => { 
+    editedNode.replaceWith(makeTeamCard(trainer))
+    editedNode = null
+})
+}
+const releasePokemon = (pokemon) => {
+  fetch(`${POKEMONS_URL}/${pokemon.id}`, {
+    method: "DELETE"
+  })
+  .then(res => res.json())
+  .then(trainer => {
+    editedNode.replaceWith(makeTeamCard(trainer))
+    editedNode = null
+  })
+}
+const editForm = document.getElementById("edit-form");
+editForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  editForm.style.display = "none"
+  trainer = {
+    name: e.target.name.value,
+    id: e.target["trainer-id"].value
+  }
+  updateTrainer(trainer); 
+})
+const updateTrainer = (trainer) => {
+  fetch(`http://localhost:3000/trainers/${trainer.id}`, {
+    method: "PATCH",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify({trainer})
+  })
+  .then(res => res.json())
+  .then(data => {
+    editedNode.replaceWith(makeTeamCard(data))
+    editedNode = null
+  })
+}
